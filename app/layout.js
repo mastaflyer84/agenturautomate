8527fea30fc388ca4f5fdc4f40f9266b4969e6d3
mastaflyer84 <sfink2@icloud.com>
2026-02-23 14:50:26 +0100

    thme korektur

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,16 @@
-﻿import "./globals.css";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+﻿// app/layout.js
+import "./globals.css";
 
 export const metadata = {
-  title: "AgenturAutomate – KI f&uuml;r Marketing-Agenturen",
-  description: "Wissens-Chatbot & Angebotsgenerator f&uuml;r Marketing-Agenturen."
+  title: "AgenturAutomate",
+  description: "Automatisierte Tools für Agenturen"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="de">
       <body>
-        <Navbar />
         {children}
-        <Footer />
       </body>
     </html>
   );
