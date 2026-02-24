497c3c2a51dca6ebf03109a1dcdfbde89cfb17d4
mastaflyer84 <sfink2@icloud.com>
2026-02-24 09:33:08 +0100

    link in page.jsx korrigiert

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
-﻿import Hero from "@/components/Hero";
-import Features from "@/components/Features";
-import Footer from "@/components/Footer";
+﻿import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
